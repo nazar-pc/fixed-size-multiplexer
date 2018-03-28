5fd30a47b72d0d3cb4b1d7167283190e8606903e
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2018-03-28 17:32:20 +0300

    Fixed minified build

--- src/index.js
+++ src/index.js
@@ -5,20 +5,6 @@
  * @license 0BSD
  */
 (function(){
-  var to_export;
-  to_export = {
-    'Multiplexer': Multiplexer,
-    'Demultiplexer': Demultiplexer
-  };
-  if (typeof define === 'function' && define['amd']) {
-    define(function(){
-      return to_export;
-    });
-  } else if (typeof exports === 'object') {
-    module.exports = to_export;
-  } else {
-    this['fixed_size_multiplexer'] = to_export;
-  }
   /**
    * @param {!Uint8Array} array
    *
@@ -66,154 +52,165 @@
     }
     object._buffer = new Uint8Array(0);
   }
-  /**
-   * Multiplexes data chunks into blocks of fixed size
-   *
-   * @constructor
-   *
-   * @param {number} max_data_length	Max size of data in bytes (influences data header size)
-   * @param {number} block_size		Size of one block of data
-   */
-  function Multiplexer(max_data_length, block_size){
-    if (!(this instanceof Multiplexer)) {
-      return new Multiplexer(max_data_length, block_size);
-    }
-    initialize(this, max_data_length, block_size);
-    this._empty = true;
-  }
-  Multiplexer.prototype = {
+  function Wrapper(){
     /**
-     * @param {!Uint8Array} data
+     * Multiplexes data chunks into blocks of fixed size
+     *
+     * @constructor
+     *
+     * @param {number} max_data_length	Max size of data in bytes (influences data header size)
+     * @param {number} block_size		Size of one block of data
      */
-    'feed': function(data){
-      var x$, new_buffer;
-      x$ = new_buffer = new Uint8Array(this._buffer.length + this._block_size_header_bytes + data.length);
-      x$.set(this._buffer);
-      x$.set(number_to_uint_array(data.length, this._block_size_header_bytes), this._buffer.length);
-      x$.set(data, this._block_size_header_bytes + this._buffer.length);
-      this._buffer = new_buffer;
-      this._empty = false;
-    }
-    /**
-     * @return {boolean}
-     */,
-    'have_more_blocks': function(){
-      return !this._empty;
-    }
-    /**
-     * @return {!Uint8Array}
-     */,
-    'get_block': function(){
-      var block, empty_headers_to_add, x$, new_buffer;
-      block = new Uint8Array(this._block_size);
-      if (this.have_more_blocks()) {
-        if (this._block_size > this._buffer.length) {
-          empty_headers_to_add = Math.ceil((this._block_size - this._buffer.length) / this._block_size_header_bytes);
-          x$ = new_buffer = new Uint8Array(this._buffer.length + empty_headers_to_add * this._block_size_header_bytes);
-          x$.set(this._buffer);
-          this._buffer = new_buffer;
-          this._empty = true;
-        }
-        block.set(this._buffer.subarray(0, this._block_size));
-        this._buffer = this._buffer.subarray(this._block_size);
+    function Multiplexer(max_data_length, block_size){
+      if (!(this instanceof Multiplexer)) {
+        return new Multiplexer(max_data_length, block_size);
       }
-      return block;
+      initialize(this, max_data_length, block_size);
+      this._empty = true;
     }
-  };
-  Object.defineProperty(Multiplexer.prototype, 'constructor', {
-    enumerable: false,
-    value: Multiplexer
-  });
-  /**
-   * Demultiplexes data chunks from blocks of fixed size
-   *
-   * @constructor
-   *
-   * @param {number} max_data_length	Max size of data in bytes (influences data header size)
-   * @param {number} block_size		Size of one block of data
-   */
-  function Demultiplexer(max_data_length, block_size){
-    if (!(this instanceof Demultiplexer)) {
-      return new Demultiplexer(max_data_length, block_size);
-    }
-    initialize(this, max_data_length, block_size);
-    this._pending_blocks = [];
-  }
-  Demultiplexer.prototype = {
+    Multiplexer.prototype = {
+      /**
+       * @param {!Uint8Array} data
+       */
+      'feed': function(data){
+        var x$, new_buffer;
+        x$ = new_buffer = new Uint8Array(this._buffer.length + this._block_size_header_bytes + data.length);
+        x$.set(this._buffer);
+        x$.set(number_to_uint_array(data.length, this._block_size_header_bytes), this._buffer.length);
+        x$.set(data, this._block_size_header_bytes + this._buffer.length);
+        this._buffer = new_buffer;
+        this._empty = false;
+      }
+      /**
+       * @return {boolean}
+       */,
+      'have_more_blocks': function(){
+        return !this._empty;
+      }
+      /**
+       * @return {!Uint8Array}
+       */,
+      'get_block': function(){
+        var block, empty_headers_to_add, x$, new_buffer;
+        block = new Uint8Array(this._block_size);
+        if (this['have_more_blocks']()) {
+          if (this._block_size > this._buffer.length) {
+            empty_headers_to_add = Math.ceil((this._block_size - this._buffer.length) / this._block_size_header_bytes);
+            x$ = new_buffer = new Uint8Array(this._buffer.length + empty_headers_to_add * this._block_size_header_bytes);
+            x$.set(this._buffer);
+            this._buffer = new_buffer;
+            this._empty = true;
+          }
+          block.set(this._buffer.subarray(0, this._block_size));
+          this._buffer = this._buffer.subarray(this._block_size);
+        }
+        return block;
+      }
+    };
+    Object.defineProperty(Multiplexer.prototype, 'constructor', {
+      value: Multiplexer
+    });
     /**
-     * @param {!Uint8Array} block
+     * Demultiplexes data chunks from blocks of fixed size
+     *
+     * @constructor
+     *
+     * @param {number} max_data_length	Max size of data in bytes (influences data header size)
+     * @param {number} block_size		Size of one block of data
      */
-    'feed': function(block){
-      if (this.have_more_data()) {
-        this._pending_blocks.push(block);
-      } else {
-        this._feed(block);
+    function Demultiplexer(max_data_length, block_size){
+      if (!(this instanceof Demultiplexer)) {
+        return new Demultiplexer(max_data_length, block_size);
       }
+      initialize(this, max_data_length, block_size);
+      this._pending_blocks = [];
     }
-    /**
-     * @param {!Uint8Array} block
-     */,
-    _feed: function(block){
-      var x$, new_buffer;
-      x$ = new_buffer = new Uint8Array(this._buffer.length + block.length);
-      x$.set(this._buffer);
-      x$.set(block, this._buffer.length);
-      this._buffer = new_buffer;
-    }
-    /**
-     * @return {boolean}
-     */,
-    'have_more_data': function(){
-      this._fill_buffer();
-      return this._have_more_data();
-    }
-    /**
-     * @return {boolean}
-     */,
-    _have_more_data: function(){
-      var data_length;
-      for (;;) {
-        if (this._buffer.length <= this._block_size_header_bytes) {
-          return false;
-        }
-        data_length = uint_array_to_number(this._buffer.subarray(0, this._block_size_header_bytes));
-        if (!data_length) {
-          this._buffer = this._buffer.subarray(this._block_size_header_bytes);
+    Demultiplexer.prototype = {
+      /**
+       * @param {!Uint8Array} block
+       */
+      'feed': function(block){
+        if (this['have_more_data']()) {
+          this._pending_blocks.push(block);
         } else {
-          break;
+          this._feed(block);
         }
       }
-      return this._buffer.length >= this._block_size_header_bytes + data_length;
-    }
-    /**
-     * @return {Uint8Array}
-     */,
-    'get_data': function(){
-      var data_length, data;
-      if (!this.have_more_data()) {
-        return null;
+      /**
+       * @param {!Uint8Array} block
+       */,
+      _feed: function(block){
+        var x$, new_buffer;
+        x$ = new_buffer = new Uint8Array(this._buffer.length + block.length);
+        x$.set(this._buffer);
+        x$.set(block, this._buffer.length);
+        this._buffer = new_buffer;
       }
-      data_length = uint_array_to_number(this._buffer.subarray(0, this._block_size_header_bytes));
-      data = this._buffer.subarray(this._block_size_header_bytes, this._block_size_header_bytes + data_length);
-      this._buffer = this._buffer.subarray(this._block_size_header_bytes + data_length);
-      return data;
-    },
-    _fill_buffer: function(){
-      var data_length;
-      for (;;) {
-        if (this._have_more_data() || !this._pending_blocks.length) {
-          break;
+      /**
+       * @return {boolean}
+       */,
+      'have_more_data': function(){
+        this._fill_buffer();
+        return this._have_more_data();
+      }
+      /**
+       * @return {boolean}
+       */,
+      _have_more_data: function(){
+        var data_length;
+        for (;;) {
+          if (this._buffer.length <= this._block_size_header_bytes) {
+            return false;
+          }
+          data_length = uint_array_to_number(this._buffer.subarray(0, this._block_size_header_bytes));
+          if (!data_length) {
+            this._buffer = this._buffer.subarray(this._block_size_header_bytes);
+          } else {
+            break;
+          }
+        }
+        return this._buffer.length >= this._block_size_header_bytes + data_length;
+      }
+      /**
+       * @return {Uint8Array}
+       */,
+      'get_data': function(){
+        var data_length, data;
+        if (!this['have_more_data']()) {
+          return null;
         }
         data_length = uint_array_to_number(this._buffer.subarray(0, this._block_size_header_bytes));
-        if (!data_length) {
-          this._buffer = new Uint8Array(0);
+        data = this._buffer.subarray(this._block_size_header_bytes, this._block_size_header_bytes + data_length);
+        this._buffer = this._buffer.subarray(this._block_size_header_bytes + data_length);
+        return data;
+      },
+      _fill_buffer: function(){
+        var data_length;
+        for (;;) {
+          if (this._have_more_data() || !this._pending_blocks.length) {
+            break;
+          }
+          data_length = uint_array_to_number(this._buffer.subarray(0, this._block_size_header_bytes));
+          if (!data_length) {
+            this._buffer = new Uint8Array(0);
+          }
+          this._feed(this._pending_blocks.shift());
         }
-        this._feed(this._pending_blocks.shift());
       }
-    }
-  };
-  Object.defineProperty(Demultiplexer.prototype, 'constructor', {
-    enumerable: false,
-    value: Demultiplexer
-  });
+    };
+    Object.defineProperty(Demultiplexer.prototype, 'constructor', {
+      value: Demultiplexer
+    });
+    return {
+      'Multiplexer': Multiplexer,
+      'Demultiplexer': Demultiplexer
+    };
+  }
+  if (typeof define === 'function' && define['amd']) {
+    define(Wrapper);
+  } else if (typeof exports === 'object') {
+    module.exports = Wrapper();
+  } else {
+    this['fixed_size_multiplexer'] = Wrapper();
+  }
 }).call(this);
--- src/index.ls
+++ src/index.ls
@@ -3,20 +3,6 @@
  * @author  Nazar Mokrynskyi <nazar@mokrynskyi.com>
  * @license 0BSD
  */
-to_export	=
-	'Multiplexer'	: Multiplexer
-	'Demultiplexer'	: Demultiplexer
-
-if typeof define == 'function' && define['amd']
-	# AMD
-	define(-> to_export)
-else if typeof exports == 'object'
-	# CommonJS
-	module.exports = to_export
-else
-	# Browser globals
-	@'fixed_size_multiplexer' = to_export
-
 /**
  * @param {!Uint8Array} array
  *
@@ -57,122 +43,137 @@ function number_to_uint_array (number, length)
 		max_data_length /= 2**8
 	object._buffer					= new Uint8Array(0)
 
-/**
- * Multiplexes data chunks into blocks of fixed size
- *
- * @constructor
- *
- * @param {number} max_data_length	Max size of data in bytes (influences data header size)
- * @param {number} block_size		Size of one block of data
- */
-!function Multiplexer (max_data_length, block_size)
-	if !(@ instanceof Multiplexer)
-		return new Multiplexer(max_data_length, block_size)
+function Wrapper
+	/**
+	 * Multiplexes data chunks into blocks of fixed size
+	 *
+	 * @constructor
+	 *
+	 * @param {number} max_data_length	Max size of data in bytes (influences data header size)
+	 * @param {number} block_size		Size of one block of data
+	 */
+	!function Multiplexer (max_data_length, block_size)
+		if !(@ instanceof Multiplexer)
+			return new Multiplexer(max_data_length, block_size)
 
-	initialize(@, max_data_length, block_size)
-	@_empty	= true
+		initialize(@, max_data_length, block_size)
+		@_empty	= true
 
-Multiplexer:: =
-	/**
-	 * @param {!Uint8Array} data
-	 */
-	'feed' : (data) !->
-		new_buffer	= new Uint8Array(@_buffer.length + @_block_size_header_bytes + data.length)
-			..set(@_buffer)
-			..set(number_to_uint_array(data.length, @_block_size_header_bytes), @_buffer.length)
-			..set(data, @_block_size_header_bytes + @_buffer.length)
-		@_buffer	= new_buffer
-		@_empty		= false
-	/**
-	 * @return {boolean}
-	 */
-	'have_more_blocks' : ->
-		!@_empty
-	/**
-	 * @return {!Uint8Array}
-	 */
-	'get_block' : ->
-		block	= new Uint8Array(@_block_size)
-		if @have_more_blocks()
-			if @_block_size > @_buffer.length
-				empty_headers_to_add	= Math.ceil((@_block_size - @_buffer.length) / @_block_size_header_bytes)
-				new_buffer				= new Uint8Array(@_buffer.length + empty_headers_to_add * @_block_size_header_bytes)
-					..set(@_buffer)
-				@_buffer				= new_buffer
-				@_empty					= true
-			block.set(@_buffer.subarray(0, @_block_size))
-			@_buffer	= @_buffer.subarray(@_block_size)
-		block
+	Multiplexer:: =
+		/**
+		 * @param {!Uint8Array} data
+		 */
+		'feed' : (data) !->
+			new_buffer	= new Uint8Array(@_buffer.length + @_block_size_header_bytes + data.length)
+				..set(@_buffer)
+				..set(number_to_uint_array(data.length, @_block_size_header_bytes), @_buffer.length)
+				..set(data, @_block_size_header_bytes + @_buffer.length)
+			@_buffer	= new_buffer
+			@_empty		= false
+		/**
+		 * @return {boolean}
+		 */
+		'have_more_blocks' : ->
+			!@_empty
+		/**
+		 * @return {!Uint8Array}
+		 */
+		'get_block' : ->
+			block	= new Uint8Array(@_block_size)
+			if @'have_more_blocks'()
+				if @_block_size > @_buffer.length
+					empty_headers_to_add	= Math.ceil((@_block_size - @_buffer.length) / @_block_size_header_bytes)
+					new_buffer				= new Uint8Array(@_buffer.length + empty_headers_to_add * @_block_size_header_bytes)
+						..set(@_buffer)
+					@_buffer				= new_buffer
+					@_empty					= true
+				block.set(@_buffer.subarray(0, @_block_size))
+				@_buffer	= @_buffer.subarray(@_block_size)
+			block
 
-Object.defineProperty(Multiplexer::, 'constructor', {enumerable: false, value: Multiplexer})
-/**
- * Demultiplexes data chunks from blocks of fixed size
- *
- * @constructor
- *
- * @param {number} max_data_length	Max size of data in bytes (influences data header size)
- * @param {number} block_size		Size of one block of data
- */
-!function Demultiplexer (max_data_length, block_size)
-	if !(@ instanceof Demultiplexer)
-		return new Demultiplexer(max_data_length, block_size)
+	Object.defineProperty(Multiplexer::, 'constructor', {value: Multiplexer})
+	/**
+	 * Demultiplexes data chunks from blocks of fixed size
+	 *
+	 * @constructor
+	 *
+	 * @param {number} max_data_length	Max size of data in bytes (influences data header size)
+	 * @param {number} block_size		Size of one block of data
+	 */
+	!function Demultiplexer (max_data_length, block_size)
+		if !(@ instanceof Demultiplexer)
+			return new Demultiplexer(max_data_length, block_size)
 
-	initialize(@, max_data_length, block_size)
-	@_pending_blocks	= []
+		initialize(@, max_data_length, block_size)
+		@_pending_blocks	= []
 
-Demultiplexer:: =
-	/**
-	 * @param {!Uint8Array} block
-	 */
-	'feed' : (block) !->
-		if @have_more_data()
-			@_pending_blocks.push(block)
-		else
-			@_feed(block)
-	/**
-	 * @param {!Uint8Array} block
-	 */
-	_feed : (block) !->
-		new_buffer	= new Uint8Array(@_buffer.length + block.length)
-			..set(@_buffer)
-			..set(block, @_buffer.length)
-		@_buffer	= new_buffer
-	/**
-	 * @return {boolean}
-	 */
-	'have_more_data' : ->
-		@_fill_buffer()
-		@_have_more_data()
-	/**
-	 * @return {boolean}
-	 */
-	_have_more_data : ->
-		loop
-			if @_buffer.length <= @_block_size_header_bytes
-				return false
-			data_length	= uint_array_to_number(@_buffer.subarray(0, @_block_size_header_bytes))
-			if !data_length
-				@_buffer	= @_buffer.subarray(@_block_size_header_bytes)
+	Demultiplexer:: =
+		/**
+		 * @param {!Uint8Array} block
+		 */
+		'feed' : (block) !->
+			if @'have_more_data'()
+				@_pending_blocks.push(block)
 			else
-				break
-		@_buffer.length >= @_block_size_header_bytes + data_length
-	/**
-	 * @return {Uint8Array}
-	 */
-	'get_data' : ->
-		if !@have_more_data()
-			return null
-		data_length	= uint_array_to_number(@_buffer.subarray(0, @_block_size_header_bytes))
-		data		= @_buffer.subarray(@_block_size_header_bytes, @_block_size_header_bytes + data_length)
-		@_buffer	= @_buffer.subarray(@_block_size_header_bytes + data_length)
-		data
-	_fill_buffer : !->
-		loop
-			if @_have_more_data() || !@_pending_blocks.length
-				break
+				@_feed(block)
+		/**
+		 * @param {!Uint8Array} block
+		 */
+		_feed : (block) !->
+			new_buffer	= new Uint8Array(@_buffer.length + block.length)
+				..set(@_buffer)
+				..set(block, @_buffer.length)
+			@_buffer	= new_buffer
+		/**
+		 * @return {boolean}
+		 */
+		'have_more_data' : ->
+			@_fill_buffer()
+			@_have_more_data()
+		/**
+		 * @return {boolean}
+		 */
+		_have_more_data : ->
+			loop
+				if @_buffer.length <= @_block_size_header_bytes
+					return false
+				data_length	= uint_array_to_number(@_buffer.subarray(0, @_block_size_header_bytes))
+				if !data_length
+					@_buffer	= @_buffer.subarray(@_block_size_header_bytes)
+				else
+					break
+			@_buffer.length >= @_block_size_header_bytes + data_length
+		/**
+		 * @return {Uint8Array}
+		 */
+		'get_data' : ->
+			if !@'have_more_data'()
+				return null
 			data_length	= uint_array_to_number(@_buffer.subarray(0, @_block_size_header_bytes))
-			if !data_length
-				@_buffer	= new Uint8Array(0)
-			@_feed(@_pending_blocks.shift())
+			data		= @_buffer.subarray(@_block_size_header_bytes, @_block_size_header_bytes + data_length)
+			@_buffer	= @_buffer.subarray(@_block_size_header_bytes + data_length)
+			data
+		_fill_buffer : !->
+			loop
+				if @_have_more_data() || !@_pending_blocks.length
+					break
+				data_length	= uint_array_to_number(@_buffer.subarray(0, @_block_size_header_bytes))
+				if !data_length
+					@_buffer	= new Uint8Array(0)
+				@_feed(@_pending_blocks.shift())
+	Object.defineProperty(Demultiplexer::, 'constructor', {value: Demultiplexer})
 
-Object.defineProperty(Demultiplexer::, 'constructor', {enumerable: false, value: Demultiplexer})
+	{
+		'Multiplexer'	: Multiplexer
+		'Demultiplexer'	: Demultiplexer
+	}
+
+if typeof define == 'function' && define['amd']
+	# AMD
+	define(Wrapper)
+else if typeof exports == 'object'
+	# CommonJS
+	module.exports = Wrapper()
+else
+	# Browser globals
+	@'fixed_size_multiplexer' = Wrapper()
